86c73e058d9f9f5a7511f844b241bc52f723e08e
Yan <chenyan2002@gmail.com>
2020-03-28 21:44:11 -0700

    Make things interactive (#2)
    * first version of interactive computing
    
    * make use of fake query calls
    
    * refactor
    
    * refactor
    
    Co-authored-by: chenyan-dfinity <yan.chen@dfinity.org>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import logo from 'ic:canisters/logo';
-import * as UI from './idl-ui';
+import { IDL, UI, UICore } from 'ic:userlib';
 import './logo.css';
+
+function Candid() {
+ const Exp = IDL.Rec()
+ const List = IDL.Rec()
+ const Statement = IDL.Rec()
+ List.fill(IDL.Opt(IDL.Record({'Statement': Statement, 'More': List})))
+ const Statements = List
+ Exp.fill(
+  IDL.Variant({'+': IDL.Record({'e1': Exp, 'e2': Exp}), 'Int': IDL.Int,
+               'Var': IDL.Text}))
+ Statement.fill(
+  IDL.Variant({'repeat': IDL.Record({'N': IDL.Nat, 'Block': Statements}),
+   'home': IDL.Null, 'left': IDL.Null, 'block': Statements, 'forward': Exp,
+   'right': IDL.Null}))
+ const Coord = IDL.Record({'x': IDL.Int, 'y': IDL.Int})
+ const Object =
+  IDL.Variant({'line': IDL.Record({'end': Coord, 'start': Coord})})
+ return IDL.Func([Statement], [], []);
+};
+
+class ExpRender extends IDL.Visitor {
+  visitType(t, d) {
+    const input = document.createElement('input');
+    input.classList.add('argument');
+    input.placeholder = t.display();
+    return inputBox(t, { input });
+  }  
+}
+
+function renderInput(t) {
+  return t.accept(new UI.Render(), null);
+}
 
 const N = 600;
 
-async function render(ctx) {
-  const res = await logo.output();
+async function renderCanvas(ctx, res) {
   const objects = res[0];
   const x = res[1].toNumber();
   const y = res[2].toNumber();
@@ -27,38 +58,47 @@ async function render(ctx) {
   ctx.translate(x,y);
   ctx.rotate(-dir*Math.PI/180);
   ctx.beginPath();
-  ctx.moveTo(-20,-20);
+  ctx.moveTo(-10,-10);
   ctx.lineTo(0,0);
-  ctx.lineTo(-20,20);
+  ctx.lineTo(-10,10);
   ctx.stroke();
   ctx.rotate(dir*Math.PI/180);  
   ctx.translate(-x,-y);
 }
 
+async function parse(inputs, ctx, fake) {
+  const args = inputs.map(arg => arg.parse());
+  const isReject = inputs.some(arg => arg.isRejected());
+  if (isReject) {
+    return;
+  }
+  let res;
+  if (fake) {
+    res = await logo.fakeEval(...args);
+  } else {
+    await logo.eval(...args);
+    res = await logo.output();
+  }
+  await renderCanvas(ctx, res);
+}
+
 function renderUI(func, dom, ctx) {
   const inputs = [];
-  const container = document.createElement('div');
+  
+  let container = document.createElement('div');
   func.argTypes.forEach(arg => {
-    const input = UI.renderInput(arg);
+    const input = renderInput(arg);
     inputs.push(input);
     input.render(container);
   });
   dom.appendChild(container);
-
+  
   dom.appendChild(document.createElement('div'));
-  const draw = document.createElement('button');
+  const draw = document.createElement('button');  
   draw.innerText = 'Draw';
   dom.appendChild(draw);
   draw.addEventListener('click', () => {
-    const args = inputs.map(arg => arg.parse());
-    const isReject = inputs.some(arg => arg.isRejected());
-    if (isReject) {
-      return;
-    }
-    (async () => {
-      await logo.eval(...args);
-      await render(ctx);
-    })();
+    parse(inputs, ctx, false);
   });
   
   dom.appendChild(document.createElement('div'));
@@ -67,25 +107,53 @@ function renderUI(func, dom, ctx) {
   dom.appendChild(clear);
   clear.addEventListener('click', () => {
     (async () => {
-      await logo.eval({home:null});
-      await render(ctx);
+      await logo.clear();
+      const res = await logo.output();
+      await renderCanvas(ctx, res);
     })();
   });
+
+  // Customize UI
+  UI.Render.prototype.visitType = (t,d) => {
+    const input = document.createElement('input');
+    input.classList.add('argument');
+    input.placeholder = t.display();
+    input.addEventListener('change', () => { parse(inputs, ctx, true); });
+    return UI.inputBox(t, { input });
+  };
+  UI.Render.prototype.visitNull = (t,d) => {
+    const input = UI.inputBox(t, {});
+    // This is a hack to make sure async parse is run after return
+    (async () => {
+      await parse(inputs, ctx, true);
+    })().catch(err => {
+      console.log("retry");
+      setTimeout(() => { parse(inputs, ctx, true) }, 200);
+    });
+    return input;
+  };
 }
 
 async function init() {
   const canvas = document.createElement("canvas");
   canvas.width = N;
   canvas.height = N;
-  document.body.appendChild(canvas);
 
+  const left = document.createElement('div');  
   const ctx = canvas.getContext('2d');
-  render(ctx);
+  const res = await logo.output();
+  renderCanvas(ctx, res);
+  left.appendChild(canvas);
   
-  const func = logo.__actorInterface()['eval'];
-  const div = document.createElement('div');
-  renderUI(func, div, ctx);
-  document.body.appendChild(div);
+  const right = document.createElement('div');
+  const func = logo.__actorInterface()['eval'];  
+  renderUI(func, right, ctx);
+
+  const box = document.createElement('div');
+  box.style.display = 'flex';
+  box.appendChild(left);
+  box.appendChild(right);
+  document.body.appendChild(box);  
 }
 
 init();
--- src/main.mo
+++ src/main.mo
@@ -66,7 +66,6 @@ class Evaluator() {
         switch stat {
         case (#home) {
                  pos.home();
-                 objects.clear();
              };
         case (#forward(exp)) {
                  let step = evalExp(env, exp);
@@ -121,11 +120,20 @@ func initEnv(): Env {
 
 actor {
     let E = Evaluator();
+    public func clear(): async () {
+        E.pos.home();
+        E.objects.clear();
+    };
     public func eval(stat:Statement): async () {
         let env = initEnv();
         E.eval(env, stat);
     };
-    public func evalExp(exp:Exp): async Int {
+    public query func fakeEval(stat:Statement): async ([Object], Int, Int, Int) {
+        let env = initEnv();
+        E.eval(env, stat);
+        (E.objects.toArray(), E.pos.x, E.pos.y, E.pos.dir)
+    };    
+    public query func evalExp(exp:Exp): async Int {
         let env = initEnv();
         E.evalExp(env, exp)
     };
