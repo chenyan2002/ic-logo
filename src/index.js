b98c00d7d0f1982453985befe2e9f8e40fd46025
Yan <chenyan2002@gmail.com>
2020-04-06 12:11:17 -0700

    customize candid UI (#3)
    * customize candid UI
    
    * add expRender
    
    * add fps. We can achieve 15fps
    
    * refactor checkpoint
    
    * checkpoint
    
    * refactor
    
    * patch gitpod
    
    Co-authored-by: chenyan-dfinity <yan.chen@dfinity.org>

--- src/index.js
+++ src/index.js
@@ -1,96 +1,10 @@
 import logo from 'ic:canisters/logo';
-import { IDL, UI, UICore } from 'ic:userlib';
+import * as UI from './ui';
 import './logo.css';
 
-function Candid() {
- const Exp = IDL.Rec()
- const List = IDL.Rec()
- const Statement = IDL.Rec()
- List.fill(IDL.Opt(IDL.Record({'Statement': Statement, 'More': List})))
- const Statements = List
- Exp.fill(
-  IDL.Variant({'+': IDL.Record({'e1': Exp, 'e2': Exp}), 'Int': IDL.Int,
-               'Var': IDL.Text}))
- Statement.fill(
-  IDL.Variant({'repeat': IDL.Record({'N': IDL.Nat, 'Block': Statements}),
-   'home': IDL.Null, 'left': IDL.Null, 'block': Statements, 'forward': Exp,
-   'right': IDL.Null}))
- const Coord = IDL.Record({'x': IDL.Int, 'y': IDL.Int})
- const Object =
-  IDL.Variant({'line': IDL.Record({'end': Coord, 'start': Coord})})
- return IDL.Func([Statement], [], []);
-};
-
-class ExpRender extends IDL.Visitor {
-  visitType(t, d) {
-    const input = document.createElement('input');
-    input.classList.add('argument');
-    input.placeholder = t.display();
-    return inputBox(t, { input });
-  }  
-}
-
-function renderInput(t) {
-  return t.accept(new UI.Render(), null);
-}
-
-const N = 600;
-
-async function renderCanvas(ctx, res) {
-  const objects = res[0];
-  const x = res[1].toNumber();
-  const y = res[2].toNumber();
-  const dir = res[3].toNumber();
-  ctx.clearRect(0, 0, N, N);
-  ctx.strokeStyle = '#000000';
-  ctx.lineWidth = 2;
-  for (const obj of objects) {
-    const start = obj.line.start;
-    const end = obj.line.end;
-    ctx.beginPath();  
-    ctx.moveTo(start.x, start.y);
-    ctx.lineTo(end.x, end.y);
-    ctx.stroke();
-  };
-  
-  ctx.strokeStyle = 'green';
-  ctx.lineWidth = 4;
-  ctx.translate(x,y);
-  ctx.rotate(-dir*Math.PI/180);
-  ctx.beginPath();
-  ctx.moveTo(-10,-10);
-  ctx.lineTo(0,0);
-  ctx.lineTo(-10,10);
-  ctx.stroke();
-  ctx.rotate(dir*Math.PI/180);  
-  ctx.translate(-x,-y);
-}
-
-async function parse(inputs, ctx, fake) {
-  const args = inputs.map(arg => arg.parse());
-  const isReject = inputs.some(arg => arg.isRejected());
-  if (isReject) {
-    return;
-  }
-  let res;
-  if (fake) {
-    res = await logo.fakeEval(...args);
-  } else {
-    await logo.eval(...args);
-    res = await logo.output();
-  }
-  await renderCanvas(ctx, res);
-}
-
-function renderUI(func, dom, ctx) {
-  const inputs = [];
-  
+function renderUI(dom) {  
   let container = document.createElement('div');
-  func.argTypes.forEach(arg => {
-    const input = renderInput(arg);
-    inputs.push(input);
-    input.render(container);
-  });
+  UI.inputs.forEach(arg => arg.render(container));
   dom.appendChild(container);
   
   dom.appendChild(document.createElement('div'));
@@ -98,7 +12,7 @@ function renderUI(func, dom, ctx) {
   draw.innerText = 'Draw';
   dom.appendChild(draw);
   draw.addEventListener('click', () => {
-    parse(inputs, ctx, false);
+    UI.parse(false);
   });
   
   dom.appendChild(document.createElement('div'));
@@ -109,51 +23,29 @@ function renderUI(func, dom, ctx) {
     (async () => {
       await logo.clear();
       const res = await logo.output();
-      await renderCanvas(ctx, res);
+      await UI.renderCanvas(res);
     })();
   });
-
-  // Customize UI
-  UI.Render.prototype.visitType = (t,d) => {
-    const input = document.createElement('input');
-    input.classList.add('argument');
-    input.placeholder = t.display();
-    input.addEventListener('change', () => { parse(inputs, ctx, true); });
-    return UI.inputBox(t, { input });
-  };
-  UI.Render.prototype.visitNull = (t,d) => {
-    const input = UI.inputBox(t, {});
-    // This is a hack to make sure async parse is run after return
-    (async () => {
-      await parse(inputs, ctx, true);
-    })().catch(err => {
-      console.log("retry");
-      setTimeout(() => { parse(inputs, ctx, true) }, 200);
-    });
-    return input;
-  };
 }
 
 async function init() {
-  const canvas = document.createElement("canvas");
-  canvas.width = N;
-  canvas.height = N;
-
+  document.body.appendChild(UI.stats.dom);
+  
   const left = document.createElement('div');  
-  const ctx = canvas.getContext('2d');
-  const res = await logo.output();
-  renderCanvas(ctx, res);
-  left.appendChild(canvas);
+  left.appendChild(UI.canvas);
   
   const right = document.createElement('div');
-  const func = logo.__actorInterface()['eval'];  
-  renderUI(func, right, ctx);
+  renderUI(right);
 
   const box = document.createElement('div');
   box.style.display = 'flex';
   box.appendChild(left);
   box.appendChild(right);
-  document.body.appendChild(box);  
+  document.body.appendChild(box);
+
+  const res = await logo.output();
+  UI.renderCanvas(res);
 }
 
 init();
+
